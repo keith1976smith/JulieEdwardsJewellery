5760643a116bb2e11e96274468dcff746eb5ea7a
Keith Smith <ksmith@sportingindex.com>
2013-09-25 17:31:38 +0100

    Added a contact page with an input form to submit messages, plus all related restyling in bootstrap. Still ongoing at this point.

---
+++ routes/contact.js
@@ -0,0 +1,3 @@
+﻿exports.index = function(req, res) {
+    res.render('contact', { subheading : "Contact", subtext : "Please complete the form below with details of your requirements and I will get back to you as soon as possible. Thank you!"});
+}
