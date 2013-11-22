b30fdd286b9c74c270a32ee829f8f2755f330915
Keith Smith <ksmith@sportingindex.com>
2013-11-22 17:01:00 +0000

    Have brought in all of Bootstrap and restructured the stylesheet physical dirs. Everything (bootstrap, custom and animations) now compiles down into a single file. Eventually, the redundant parts of Bootstrap will be removed, but for now it's there in its entirety for development.

--- routes/contact.js
+++ routes/contact.js
@@ -1,3 +1,7 @@
 ﻿exports.index = function(req, res) {
-    res.render('contact', { subheading : "Contact", subtext : "Please complete the form below with details of your requirements and I will get back to you as soon as possible. Thank you!"});
+    res.render('contact',
+        {
+            subheading: "Contact",
+            subtext: "Please complete the form below with details of your requirements and I will get back to you as soon as possible. Thank you!"
+        });
 }
